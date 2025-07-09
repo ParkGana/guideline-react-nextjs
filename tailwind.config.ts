7b81498d6fe315e75b918b0d83a674803837d941
ParkGana <gnchoco97@gmail.com>
2025-07-09 11:44:59 +0900

    Tailwind CSS - Color 및 Typography 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,25 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'gray-100': 'var(--gray-100)',
+        'gray-200': 'var(--gray-200)',
+        'gray-300': 'var(--gray-300)',
+        'gray-400': 'var(--gray-400)',
+        'gray-500': 'var(--gray-500)',
+        'gray-600': 'var(--gray-600)',
+        'gray-700': 'var(--gray-700)',
+        'gray-800': 'var(--gray-800)',
+        'gray-900': 'var(--gray-900)'
+      },
+      fontSize: {
+        title: ['40px', { lineHeight: '48px', fontWeight: '700' }],
+        label: ['20px', { lineHeight: '28px', fontWeight: '700' }],
+        body: ['16px', { lineHeight: '20px', fontWeight: '500' }],
+        caption: ['12px', { lineHeight: '16px', fontWeight: '500' }]
+      }
+    }
   },
   plugins: []
 };
