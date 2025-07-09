aea5ee3978e5c5871fd753ef004bfd963022d628
ParkGana <gnchoco97@gmail.com>
2025-07-09 16:57:48 +0900

    TanStack Query - 설치 및 설정

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,7 +1,12 @@
 import AuthProvider from './providers/authProvider';
+import QueryProvider from './providers/queryProvider';
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <QueryProvider>
+      <AuthProvider>{children}</AuthProvider>
+    </QueryProvider>
+  );
 };
 
 export default Provider;
