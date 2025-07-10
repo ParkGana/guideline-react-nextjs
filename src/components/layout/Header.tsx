be3a0b7030656a97d299ae65bcb4ee5bb075ad40
ParkGana <gnchoco97@gmail.com>
2025-07-10 11:23:16 +0900

    * check code *

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="sticky top-0 z-50 border-b border-gray-300 bg-gray-100">
       <div className="w-full max-w-7xl flex items-center justify-between p-4 mx-auto">
-        <p className="text-4xl font-black">Vite</p>
+        <p className="text-4xl font-black">Next.js</p>
         <ThemeButton />
       </div>
     </div>
